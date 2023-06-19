93191cdcefed8ad3beb23ba201455f4ec06a26d0
Simon <simon.louis638@gmail.com>
2023-06-19 16:57:50 +0200

    create posts

--- Front/Serveur/main.go
+++ Front/Serveur/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"io"
 	"net/http"
+	"os"
+	"github.com/google/uuid"
 )
 
 type Message struct {
@@ -11,8 +14,74 @@ type Message struct {
 	Content  string
 }
 
+type Post struct {
+	Title   string
+	Content string
+	Image   string
+}
+
 var messages []Message
+var posts []Post
 var isLiked = false
+
+func createHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		// Récupérer les données du formulaire
+		title := r.FormValue("title")
+		content := r.FormValue("content")
+
+		// Vérifier si les champs sont vides
+		if title == "" || content == "" {
+			fmt.Fprintf(w, "Veuillez remplir tous les champs")
+			return
+		}
+
+		// Vérifier si la limite de caractères est respectée
+		if len(content) > 300 {
+			fmt.Fprintf(w, "Le contenu doit contenir au maximum 300 caractères")
+			return
+		}
+
+		// Récupérer le fichier image
+		file, handler, err := r.FormFile("image")
+		if err != nil && err != http.ErrMissingFile {
+			fmt.Fprintf(w, "Erreur lors du chargement du fichier image: %v", err)
+			return
+		}
+		defer file.Close()
+
+		// Générer un identifiant unique pour le nom de fichier
+		imageID := uuid.New().String()
+
+		// Définir le chemin d'accès complet à l'image
+		imagePath := "../assets/images" + imageID + handler.Filename
+
+		// Si un fichier image a été téléchargé, le sauvegarder sur le disque
+		if handler != nil {
+			f, err := os.Create(imagePath)
+			if err != nil {
+				fmt.Fprintf(w, "Erreur lors de la sauvegarde du fichier image: %v", err)
+				return
+			}
+			defer f.Close()
+
+			_, err = io.Copy(f, file)
+			if err != nil {
+				fmt.Fprintf(w, "Erreur lors de la copie du fichier image: %v", err)
+				return
+			}
+		}
+
+		// Créer un nouveau post avec l'image
+		post := Post{Title: title, Content: content, Image: imagePath}
+
+		// Ajouter le post à la liste des posts
+		posts = append(posts, post)
+	}
+
+	// Rediriger vers la page d'accueil après la création du post
+	http.Redirect(w, r, "/", http.StatusFound)
+}
 
 func likePost(w http.ResponseWriter, r *http.Request) {
 	if isLiked {
@@ -46,7 +115,13 @@ func SendMessage(w http.ResponseWriter, r *http.Request) {
 func general(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("../templates/general.page.tmpl"))
 
-	tmpl.Execute(w, nil)
+	data := struct {
+		Posts []Post
+	}{
+		Posts: posts,
+	}
+
+	tmpl.Execute(w, data)
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
@@ -82,6 +157,7 @@ func main() {
 	http.HandleFunc("/register", register)
 	http.HandleFunc("/send", SendMessage)
 	http.HandleFunc("/mp", MP)
+	http.HandleFunc("/create", createHandler)
 	fmt.Println("Serveur démarré sur : http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
